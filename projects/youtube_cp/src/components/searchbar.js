bef2c0c59a843c7275ceca190100581211fd230d
Chen <cheneus@gmail.com>
2018-11-05 17:00:25 -0500

    update to react proj, code clean up

--- projects/youtube_cp/src/components/searchbar.js
+++ projects/youtube_cp/src/components/searchbar.js
@@ -1,27 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 // import React  from 'react'
 
 // class SearchBar extends React.Component {
 // start with functional then use class if only you need more functionality
 class SearchBar extends Component {
-	constructor (props) {
-		super(props);
-		this.state = {term: ''};
-	}
+	state = { term: '' };
 
 	render() {
 		return (
-		<div className ='search-bar'>
-		 <input 
-		 	value = {this.state.term}
-		 	// onChange={this.onInputChange}/>
-		 	onChange = {event => this.onInputChange(event.target.value)} />
-		</div>
-	)
-}
+			<div className='search-bar'>
+				<input
+					value={this.state.term}
+					// onChange={this.onInputChange}/>
+					onChange={event => this.onInputChange(event.target.value)} />
+			</div>
+		)
+	}
 
 	onInputChange(event) {
-		this.setState({term: event})
+		this.setState({ term: event })
 		this.props.onSearchTermChange(this.state.term)
 	}
 };
--- projects/youtube_cp/src/index.js
+++ projects/youtube_cp/src/index.js
@@ -1,55 +1,56 @@
 import ReactDOM from 'react-dom';
 import _ from 'lodash';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/searchbar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 import youtube from '../config'
 
-// const API_KEY = keys;
-// const API_KEY = '';
-
 // create the component
 // one component per file
 class App extends Component {
-	constructor(props) {
-		super(props);
+	// constructor(props) {
+	// 	super(props);
 
-		this.state = { 
-			videos: [],
-			selectedVideo: null
-		};
-		this.videoSearch('pokemon')
+	state = {
+		videos: [],
+		selectedVideo: null
+	};
+
+	componentWillMount() {
+		this.videoSearch("pokemon")
 	}
 
-		videoSearch= (term) => {
-			YTSearch({key: youtube, term: term}, (videos) => {
-				this.setState({
-				videos : videos,
-				selectedVideo : videos[0]
-				})
+	videoSearch = (term) => {
+		// keys comes from the config.js
+		YTSearch({ key: youtube, term: term }, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
 			})
-		}
-	
+		})
+	}
+
 	render() {
 		// delay the function
 		const videoSearch = _.debounce((term) => {
-			this.videoSearch(term) }, 500)
+			this.videoSearch(term)
+		}, 500)
 
 		return (
 			<div>
-			<SearchBar onSearchTermChange={videoSearch}/>
-				<div className='row'> 
-				<VideoDetail video={this.state.selectedVideo}/>
-				<VideoList
-					onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
-					videos={this.state.videos} />
+				<SearchBar onSearchTermChange={videoSearch} />
+				<div className='row'>
+					<VideoDetail video={this.state.selectedVideo} />
+					<VideoList
+						onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+						videos={this.state.videos} />
 				</div>
 			</div>
-		)	
+		)
 	}
-}	
+}
 
 // insert generated HTML in the page(DOM)
 // wrap in jsx syntax to create component
